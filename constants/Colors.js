36e438a50f87bf690ccd7e0c06a4d901f7f30a13
chiragraghani7 <chiragraghani7@gmail.com>
2019-06-27 10:12:37 +0530

    Initialisation of Music player App

---
+++ constants/Colors.js
@@ -0,0 +1,24 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  // Our Colors
+
+  primaryColor: '',
+  accentColor: '',
+  placeholderColor:'',
+  accentGradientStart:'',
+  accentGradientEnd:'',
+  primaryGradientStart:'',
+  primaryGradientEnd:'',
+  headingColor:''
+};
